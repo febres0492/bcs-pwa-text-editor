2c594cf6d279596c49b14da10166720284460fa6
Febres0492 <febres0492@gmail.com>
2024-07-27 16:16:35 -0400

    Implement asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -8,23 +8,37 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
+    cacheName: 'page-cache',
+    plugins: [
+        new CacheableResponsePlugin({
+            statuses: [0, 200],
+        }),
+        new ExpirationPlugin({
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+        }),
+    ],
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
+    urls: ['/index.html', '/'],
+    strategy: pageCache,
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+    // Match CSS, JS, and image files
+    ({ request }) => ['style', 'script', 'image'].includes(request.destination),
+    new StaleWhileRevalidate({
+        cacheName: 'asset-cache',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [0, 200],
+            }),
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
+            }),
+        ],
+    })
+);
