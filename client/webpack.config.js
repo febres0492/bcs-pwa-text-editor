69b85fa133619a79ca24dcffc44b5dedfc8c7466
Carlos Febres <febres0492@gmail.com>
2024-07-26 21:39:02 -0400

    Update server.js and package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,28 +3,63 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+    return {
+        mode: 'development',
+        entry: {
+            main: './src/js/index.js',
+            install: './src/js/install.js'
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './index.html',
+                title: 'Progressive Web App'
+            }),
+            new InjectManifest({
+                swSrc: './src-sw.js',
+                swDest: 'service-worker.js'
+            }),
+            new WebpackPwaManifest({
+                name: 'Progressive Web App',
+                short_name: 'PWA',
+                description: 'My awesome Progressive Web App!',
+                background_color: '#ffffff',
+                crossorigin: 'use-credentials', 
+                icons: [
+                    {
+                        src: path.resolve('src/images/logo.png'),
+                        sizes: [96, 128, 192, 256, 384, 512] 
+                    }
+                ]
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ],
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 9000,
+            open: true, // Automatically open the browser
+        },
+    };
 };
